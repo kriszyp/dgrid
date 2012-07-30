150f44595c588b0f60b7f4951cf964dc380f274b
Kris Zyp <kriszyp@gmail.com>
2012-07-30 14:07:47 -0600

    Start of compoundColumn extension plugin

---
+++ extensions/compoundColumn.js
@@ -0,0 +1,36 @@
+define(["dojo/_base/lang", "put-selector/put"], 
+	function(lang, put){
+		return function(column){
+			function render(column, container, grid, object, options, header){
+				var table = put(container, "table");
+				var row, children = column.children;
+				if(header){
+					var th = put(table, "tr th", column.label);
+					th.colSpan = children.colSpan;
+				}
+				if(children){
+					var row = put(table, "tr td", grid.createRowCells(header ? "th" : "td", function(cell, column){
+						this[header ? "renderHeaderCell" : "renderCell"](cell, column, object, options);
+					}, [column.children]), "!dgrid-row-table");
+					row.style.width = "100%"
+/*					for(var i = 0; i < children.length; i++){
+						var child = column.children[i];
+						if("label" in child){
+							if(!row){
+								row = put(table, "tr");
+							}
+							grid[header ? "renderHeaderCell" : "renderCell"](put(row, header ? "th" : "td"), child, object, options);
+						}
+					}*/
+				}
+			}
+
+			column.renderHeaderCell = function(th){
+				render(column, th, column.grid, null, null, true);
+			};
+			column.renderCell = function(object, value, td, options){
+				render(column, td, column.grid, object, options);
+			}
+			return column;
+		};
+});
